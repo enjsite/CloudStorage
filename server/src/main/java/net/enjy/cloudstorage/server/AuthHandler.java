2e5cbb873ff025d5cc24c1c6df3b7ea3b77646a3
enjsite <enjyweb@gmail.com>
2019-08-04 00:12:14 +0300

    db

--- server/src/main/java/net/enjy/cloudstorage/server/AuthHandler.java
+++ server/src/main/java/net/enjy/cloudstorage/server/AuthHandler.java
@@ -8,17 +8,41 @@ import net.enjy.cloudstorage.common.AuthOk;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AuthHandler extends ChannelInboundHandlerAdapter {
 
     private boolean authorized;
+    public Map<String, String> users = new HashMap<>();
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         if (!authorized) {
             if (msg instanceof AuthMessage) {
                 AuthMessage authMessage = (AuthMessage) msg;
-                if (authMessage.getLogin().equals("login") && (authMessage.getPassword().equals("password"))) {
+
+                try{
+                    Class.forName("org.sqlite.JDBC");
+
+                    try (Connection connection = DriverManager.getConnection("jdbc:sqlite:clients.db")) {
+                        Statement statement = connection.createStatement();
+
+                        for(Client client : readAllClients(statement)) {
+                            System.out.println(client);
+                            users.put(client.getLogin(), client.getPassword());
+                        }
+
+                    }catch (SQLException ex){
+                        System.out.println(ex.getMessage());
+                    }
+                }catch (ClassNotFoundException e){
+                    System.out.println(e.getMessage());
+                }
+
+                if (authUser(authMessage.getLogin(), authMessage.getPassword()))  {
                     String username = authMessage.getLogin();
                     authorized = true;
                     System.out.println("Authorized client");
@@ -38,6 +62,35 @@ public class AuthHandler extends ChannelInboundHandlerAdapter {
         } else {
             ctx.fireChannelRead(msg);
         }
+    }
+
+    private static Collection<Client> readAllClients(Statement statement) throws SQLException {
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM clients");
+
+        Map<Integer, Client> clientById = new HashMap<>();
+        while (resultSet.next()) {
+            int id = resultSet.getInt(1);
+            if (clientById.get(id) == null) {
+                clientById.put(id, createClient(resultSet, id));
+            }
+        }
+        return clientById.values();
+    }
+
+    private static Client createClient(ResultSet resultSet, int id) throws SQLException {
+        String login = resultSet.getString(2);
+        String password = resultSet.getString(3);
+
+        Client client = new Client();
+        client.setId(id);
+        client.setLogin(login);
+        client.setPassword(password);
+        return client;
+    }
+
+    public boolean authUser(String username, String password) {
+        String pwd = users.get(username);
+        return pwd != null && pwd.equals(password);
     }
 
     @Override
